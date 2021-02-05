2b802318f4a05d5ce2f7fc2d1d657a251ff45924
NatashaPolnaya <natashapolnaya@yandex.ru>
2021-02-05 22:01:40 +0300

    Отображение валют и их курса

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const express = require("express");
+const requestAPI = require("request");
 
 const app = express();
 const port = 3000;
 
+app.set("view engine", "hbs");
+
 app.get("/", (request, response) => {
-    response.send("It's working!!!");
+    const url = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+    requestAPI(url, (error, requesr2, data) => {
+        let model = {
+            Valute: {}
+        };
+        
+        if (error) console.log(error);
+        else {
+            model = JSON.parse(data);
+
+            model.Valute["RUS"] = {
+                ID: "R0",
+                NumCode: "0",
+                CharCode: "RUS",
+                Nominal: 1,
+                Name: "Российский рубль",
+                Value: 1,
+                Previous: 1
+            };
+
+            for (const key in model.Valute) {
+                const element = model.Valute[key];
+
+                element.Value = element.Value / element.Nominal;
+                element.DeValue = 1 / element.Value;
+            }
+        }
+
+        response.render("main", model);
+    });
 });
 
 app.get("/*", (request, response) => {
